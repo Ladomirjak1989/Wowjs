039d23edbcfc344e71ba63bcc35cdd0ab16713bd
Bettina <bettike18989@gmail.com>
2025-03-19 10:07:15 +0100

    Wowjs

--- src/components/hooks/useWow.ts
+++ src/components/hooks/useWow.ts
@@ -1,11 +1,9 @@
 'use client';
 import { useEffect } from 'react';
-import { WOW } from 'wowjs'; // ✅ Імпортуємо як default
 
 export default function useWow(): void {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      // new WOW().init(); // ✅ Викликаємо без WOW.WOW()
     }
   }, []);
 }
